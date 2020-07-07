04b7e217356c14c3a5998778ccc3dd0bec9198c9
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2020-07-07 10:39:39 +0600

    [Runner] Run adb shell without thread
    Since runners that support ADB ops are already being run in the background thread

--- app/src/main/java/io/github/muntashirakon/AppManager/runner/AdbShellRunner.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/runner/AdbShellRunner.java
@@ -16,48 +16,40 @@ public class AdbShellRunner extends Runner {
 
     @Override
     public Result run() {
-        Thread thread = new Thread(() -> {
-            try {
-                AdbShell.CommandResult result = AdbShell.run(TextUtils.join("; ", commands));
-                clear();
-                lastResult = new Result() {
-                    @Override
-                    public boolean isSuccessful() {
-                        return result.isSuccessful();
-                    }
-
-                    @Override
-                    public List<String> getOutputAsList() {
-                        return result.getOutputAsList();
-                    }
-
-                    @Override
-                    public List<String> getOutputAsList(int first_index) {
-                        return result.getOutputAsList().subList(first_index, result.getOutputAsList().size());
-                    }
-
-                    @Override
-                    public List<String> getOutputAsList(int first_index, int length) {
-                        return result.getOutputAsList().subList(first_index, first_index + length);
-                    }
-
-                    @Override
-                    public String getOutput() {
-                        return result.getOutput();
-                    }
-                };
-            } catch (InterruptedException | IOException | NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-        });
         try {
-            thread.start();
-            thread.join(1000);
-        } catch (InterruptedException e) {
+            AdbShell.CommandResult result = AdbShell.run(TextUtils.join("; ", commands));
+            clear();
+            lastResult = new Result() {
+                @Override
+                public boolean isSuccessful() {
+                    return result.isSuccessful();
+                }
+
+                @Override
+                public List<String> getOutputAsList() {
+                    return result.stdout;
+                }
+
+                @Override
+                public List<String> getOutputAsList(int first_index) {
+                    return result.stdout.subList(first_index, result.stdout.size());
+                }
+
+                @Override
+                public List<String> getOutputAsList(int first_index, int length) {
+                    return result.stdout.subList(first_index, first_index + length);
+                }
+
+                @Override
+                public String getOutput() {
+                    return result.getStdout();
+                }
+            };
+            return lastResult;
+        } catch (InterruptedException | IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
             return null;
         }
-        return lastResult;
     }
 
     @Override
