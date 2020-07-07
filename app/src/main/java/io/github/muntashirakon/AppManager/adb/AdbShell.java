516f834eb2fc17dbd921a2df66c9d2d83e84f67a
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2020-07-07 20:06:12 +0600

    [ADB] Fix ADB output being not generated for some commands

--- app/src/main/java/io/github/muntashirakon/AppManager/adb/AdbShell.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/adb/AdbShell.java
@@ -1,75 +1,103 @@
 package io.github.muntashirakon.AppManager.adb;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.tananaev.adblib.AdbConnection;
 import com.tananaev.adblib.AdbStream;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import io.github.muntashirakon.AppManager.AppManager;
+import io.github.muntashirakon.AppManager.utils.Utils;
 
 public class AdbShell {
     private static AdbConnection adbConnection;
     private static AdbStream shell;
+
     private static final String adbHost = "127.0.0.1";
     private static final int adbPort = 5555;
+    private static final File cachePath = Objects.requireNonNull(AppManager.getContext().getExternalCacheDir());
+    public static final File stdoutPath = new File(cachePath, ".stdout");
+    public static final File cmdPath = new File(cachePath, ".cmd");
+    public static final File retCodePath =  new File(cachePath, ".code");
+    private static final String formattedCmdString = "#!/bin/sh\n%s\necho $? > " + retCodePath.getAbsolutePath();
 
-    public interface CommandResult {
-        boolean isSuccessful();
-        String getOutput();
-        List<String> getOutputAsList();
+    public static class CommandResult {
+        public int returnCode;
+        public @NonNull List<String> stdout;
+
+        private CommandResult(@NonNull List<String> stdout, int returnCode) {
+            this.returnCode = returnCode;
+            this.stdout = stdout;
+        }
+
+        public boolean isSuccessful() {
+            return returnCode == 0;
+        }
+
+        public String getStdout() {
+            return TextUtils.join("\n", stdout);
+        }
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @NonNull
-    public static CommandResult run(String command)
+    synchronized public static CommandResult run(String command)
             throws IOException, InterruptedException, NoSuchAlgorithmException {
         if (adbConnection == null) {
             adbConnection = AdbConnectionManager.connect(AppManager.getContext(), adbHost, adbPort);
         }
-        if (shell == null) shell = adbConnection.open("shell:");
-        shell.write(command + "; echo $? AlsoSprachZarathustra\n");
-        String lines;
-        // Strip previous command
-        do {
-            lines = new String(shell.read(), StandardCharsets.US_ASCII);
-        } while (!lines.contains("AlsoSprachZarathustra"));
-        // Strip null
-        do {
-            lines = new String(shell.read(), StandardCharsets.US_ASCII);
-        } while (!lines.equals("^@"));
-        // Strip this command
-        do {
-            lines = new String(shell.read(), StandardCharsets.US_ASCII);
-        } while (!lines.contains("AlsoSprachZarathustra"));
-        // Get output
-        lines = new String(shell.read(), StandardCharsets.US_ASCII);
-        StringBuilder sb = new StringBuilder();
-        while (!lines.contains("AlsoSprachZarathustra")) {
-            sb.append(lines);
-            lines = new String(shell.read(), StandardCharsets.US_ASCII);
+        if (shell != null) shell.close();
+        shell = adbConnection.open("shell:");
+        if (!cachePath.exists()) cachePath.mkdir();
+        // Write command
+        try (FileOutputStream cmdStream = new FileOutputStream(cmdPath)) {
+            cmdStream.write(String.format(formattedCmdString, command).getBytes());
         }
-        int returnCode = Integer.parseInt(lines.substring(0, lines.indexOf(" AlsoSprachZarathustra")));
-        return new CommandResult() {
-            @Override
-            public boolean isSuccessful() {
-                return returnCode == 0;
+        // Execute command
+        shell.write((" /bin/sh " + cmdPath.getAbsolutePath() + " | awk '{ print $0 }' > " + stdoutPath.getAbsolutePath() + "; exit\n").getBytes(), true);
+        // Debug output
+        while (!shell.isClosed()) {
+            try {
+                Log.d("AdbShell - Raw", new String(shell.read(), StandardCharsets.US_ASCII));
+            } catch (Exception e) {
+                break;
             }
-
-            @Override
-            public String getOutput() {
-                return sb.toString();
-            }
-
-            @Override
-            public List<String> getOutputAsList() {
-                return Arrays.asList(sb.toString().split("\\r?\\n"));
-            }
-        };
-    }
-
-
+        }
+        // Wait for result
+        while (!retCodePath.exists()) Thread.sleep(500);
+        while (!stdoutPath.exists()) Thread.sleep(500);
+        // Read exit code
+        final String retCode = Utils.getFileContent(retCodePath);
+        int returnCode = -1;
+        try {
+            returnCode = Integer.parseInt(retCode);
+        } catch (Exception ignored) {}
+        // Read standard output
+        List<String> stdout = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(stdoutPath))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) stdout.add(line.trim());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Debug logs
+        Log.d("AdbShell - Command", command);
+        Log.d("AdbShell - Stdout", stdout.toString());
+        Log.d("AdbShell - Return Code", returnCode + " (" + retCode + ")");
+        // Delete files
+        stdoutPath.delete();
+        retCodePath.delete();
+        return new CommandResult(stdout, returnCode);    }
 }
