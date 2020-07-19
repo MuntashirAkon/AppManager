2162b6dff351b49267ab728dd57b2fb118a7500e
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2020-07-19 15:34:04 +0600

    [App] Make links clickable

---
+++ app/src/main/java/io/github/muntashirakon/AppManager/types/HyperlinkTextView.java
@@ -0,0 +1,32 @@
+package io.github.muntashirakon.AppManager.types;
+
+import android.content.Context;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class HyperlinkTextView extends AppCompatTextView {
+    public HyperlinkTextView(Context context) {
+        super(context);
+    }
+
+    public HyperlinkTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HyperlinkTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * Set default movement method to {@link LinkMovementMethod}
+     * @return Link movement method as the default movement method
+     */
+    @Override
+    protected MovementMethod getDefaultMovementMethod() {
+        return LinkMovementMethod.getInstance();
+    }
+}
