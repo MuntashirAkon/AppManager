def6eb8af4f8f42d82fb6fdaf1de614e8abb0607
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2020-08-17 09:05:41 +0600

    [App] Add copyright notices

--- app/src/main/java/io/github/muntashirakon/AppManager/details/LauncherIconCreator.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/details/LauncherIconCreator.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020 Muntashir Al-Islam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // This is a modified version of LauncherIconCreator.java taken
 // from https://github.com/butzist/ActivityLauncher/commit/dfb7fe271dae9379b5453bbb6e88f30a1adc94a9
 // and was authored by Adam M. Szalkowski with ISC License.
