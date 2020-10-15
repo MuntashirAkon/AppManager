b6b2336a458fca37b68dfbc87ad08c3d70c6f2f4
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2020-10-16 04:18:35 +0600

    Add script to push to mirrors

---
+++ push_to_mirrors.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+if ! git push gitlab; then
+    git remote add --mirror=push gitlab git@gitlab.com:muntashir/AppManager.git
+    git push gitlab
+fi
+
+if ! git push riseup; then
+    git remote add --mirror=push riseup git@0xacab.org:muntashir/AppManager.git
+    git push riseup
+fi
+
+if ! git push codeberg; then
+    git remote add --mirror=push codeberg git@codeberg.org:muntashir/AppManager.git
+    git push codeberg
+fi
+
+if ! git push sourcehut; then
+    git remote add --mirror=push sourcehut git@git.sr.ht:~muntashir/AppManager
+    git push sourcehut
+fi
