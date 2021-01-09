c1b4554ba61b94e7b44f01598caf64f34950b303
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2021-01-09 14:30:20 +0600

    [Refactor] Move backup/restore settings to another fragment

---
+++ app/src/main/java/io/github/muntashirakon/AppManager/settings/BackupRestorePreferences.java
@@ -0,0 +1,144 @@
+package io.github.muntashirakon.AppManager.settings;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+import io.github.muntashirakon.AppManager.R;
+import io.github.muntashirakon.AppManager.backup.BackupFlags;
+import io.github.muntashirakon.AppManager.backup.CryptoUtils;
+import io.github.muntashirakon.AppManager.backup.MetadataManager;
+import io.github.muntashirakon.AppManager.utils.AppPref;
+import io.github.muntashirakon.AppManager.utils.ArrayUtils;
+
+public class BackupRestorePreferences extends PreferenceFragmentCompat {
+    @StringRes
+    private static final int[] encryptionNames = new int[]{
+            R.string.none,
+            R.string.aes,
+            R.string.rsa,
+            R.string.ecc,
+            R.string.open_pgp_provider
+    };
+
+    SettingsActivity activity;
+    private int currentCompression;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences_backup_restore, rootKey);
+        getPreferenceManager().setPreferenceDataStore(new SettingsDataStore());
+        activity = (SettingsActivity) requireActivity();
+        // Backup compression method
+        String[] tarTypes = MetadataManager.TAR_TYPES;
+        String[] readableTarTypes = new String[]{"GZip", "BZip2"};
+        currentCompression = ArrayUtils.indexOf(tarTypes, AppPref.get(AppPref.PrefKey.PREF_BACKUP_COMPRESSION_METHOD_STR));
+        Preference compressionMethod = Objects.requireNonNull(findPreference("backup_compression_method"));
+        compressionMethod.setSummary(getString(R.string.compression_method, readableTarTypes[currentCompression == -1 ? 0 : currentCompression]));
+        compressionMethod.setOnPreferenceClickListener(preference -> {
+            new MaterialAlertDialogBuilder(activity)
+                    .setTitle(R.string.pref_compression_method)
+                    .setSingleChoiceItems(readableTarTypes, currentCompression,
+                            (dialog, which) -> currentCompression = which)
+                    .setPositiveButton(R.string.save, (dialog, which) -> {
+                        AppPref.set(AppPref.PrefKey.PREF_BACKUP_COMPRESSION_METHOD_STR, tarTypes[currentCompression]);
+                        compressionMethod.setSummary(getString(R.string.compression_method, readableTarTypes[currentCompression == -1 ? 0 : currentCompression]));
+                    })
+                    .setNegativeButton(R.string.cancel, null)
+                    .show();
+            return true;
+        });
+        // Backup flags
+        BackupFlags flags = BackupFlags.fromPref();
+        ((Preference) Objects.requireNonNull(findPreference("backup_flags"))).setOnPreferenceClickListener(preference -> {
+            new MaterialAlertDialogBuilder(activity)
+                    .setTitle(R.string.backup_options)
+                    .setMultiChoiceItems(BackupFlags.getFormattedFlagNames(activity),
+                            flags.flagsToCheckedItems(),
+                            (dialog, index, isChecked) -> {
+                                if (isChecked) {
+                                    flags.addFlag(BackupFlags.backupFlags.get(index));
+                                } else flags.removeFlag(BackupFlags.backupFlags.get(index));
+                            })
+                    .setPositiveButton(R.string.save, (dialog, which) ->
+                            AppPref.set(AppPref.PrefKey.PREF_BACKUP_FLAGS_INT, flags.getFlags()))
+                    .setNegativeButton(R.string.cancel, null)
+                    .show();
+            return true;
+        });
+        // Keystore toggle
+        SwitchPreferenceCompat backupKeyStore = Objects.requireNonNull(findPreference("backup_android_keystore"));
+        backupKeyStore.setChecked((boolean) AppPref.get(AppPref.PrefKey.PREF_BACKUP_ANDROID_KEYSTORE_BOOL));
+        // Encryption
+        ((Preference) Objects.requireNonNull(findPreference("encryption"))).setOnPreferenceClickListener(preference -> {
+            CharSequence[] encryptionNamesText = new CharSequence[encryptionNames.length];
+            for (int i = 0; i < encryptionNames.length; ++i) {
+                encryptionNamesText[i] = getString(encryptionNames[i]);
+            }
+            int choice = encModeToIndex((String) AppPref.get(AppPref.PrefKey.PREF_ENCRYPTION_STR));
+            new MaterialAlertDialogBuilder(activity)
+                    .setTitle(R.string.encryption)
+                    .setSingleChoiceItems(encryptionNamesText, choice, (dialog, which) -> {
+                        String encryptionMode = indexToEncMode(which);
+                        switch (encryptionMode) {
+                            case CryptoUtils.MODE_NO_ENCRYPTION:
+                                AppPref.set(AppPref.PrefKey.PREF_ENCRYPTION_STR, encryptionMode);
+                                break;
+                            case CryptoUtils.MODE_AES:
+                            case CryptoUtils.MODE_RSA:
+                            case CryptoUtils.MODE_ECC:
+                                // TODO(12/11/20): Implement encryption options
+                                Toast.makeText(activity, "Not implemented yet.", Toast.LENGTH_SHORT).show();
+                                break;
+                            case CryptoUtils.MODE_OPEN_PGP:
+                                AppPref.set(AppPref.PrefKey.PREF_ENCRYPTION_STR, encryptionMode);
+                                new OpenPgpKeySelectionDialogFragment().show(getParentFragmentManager(), OpenPgpKeySelectionDialogFragment.TAG);
+                        }
+                    })
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+            return true;
+        });
+    }
+
+    @CryptoUtils.Mode
+    private String indexToEncMode(int index) {
+        switch (index) {
+            default:
+            case 0:
+                return CryptoUtils.MODE_NO_ENCRYPTION;
+            case 1:
+                return CryptoUtils.MODE_AES;
+            case 2:
+                return CryptoUtils.MODE_RSA;
+            case 3:
+                return CryptoUtils.MODE_ECC;
+            case 4:
+                return CryptoUtils.MODE_OPEN_PGP;
+        }
+    }
+
+    private int encModeToIndex(@NonNull @CryptoUtils.Mode String mode) {
+        switch (mode) {
+            default:
+            case CryptoUtils.MODE_NO_ENCRYPTION:
+                return 0;
+            case CryptoUtils.MODE_AES:
+                return 1;
+            case CryptoUtils.MODE_RSA:
+                return 2;
+            case CryptoUtils.MODE_ECC:
+                return 3;
+            case CryptoUtils.MODE_OPEN_PGP:
+                return 4;
+        }
+    }
+}
--- app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
@@ -56,7 +56,6 @@ import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.collection.ArrayMap;
 import androidx.core.app.ActivityCompat;
@@ -70,9 +69,6 @@ import io.github.muntashirakon.AppManager.AppManager;
 import io.github.muntashirakon.AppManager.BuildConfig;
 import io.github.muntashirakon.AppManager.R;
 import io.github.muntashirakon.AppManager.StaticDataset;
-import io.github.muntashirakon.AppManager.backup.BackupFlags;
-import io.github.muntashirakon.AppManager.backup.CryptoUtils;
-import io.github.muntashirakon.AppManager.backup.MetadataManager;
 import io.github.muntashirakon.AppManager.intercept.ActivityInterceptor;
 import io.github.muntashirakon.AppManager.misc.Users;
 import io.github.muntashirakon.AppManager.rules.compontents.ComponentUtils;
@@ -82,7 +78,6 @@ import io.github.muntashirakon.AppManager.runner.RunnerUtils;
 import io.github.muntashirakon.AppManager.types.FullscreenDialog;
 import io.github.muntashirakon.AppManager.types.ScrollableDialogBuilder;
 import io.github.muntashirakon.AppManager.utils.AppPref;
-import io.github.muntashirakon.AppManager.utils.ArrayUtils;
 import io.github.muntashirakon.AppManager.utils.IOUtils;
 import io.github.muntashirakon.AppManager.utils.LangUtils;
 import io.github.muntashirakon.AppManager.utils.PackageUtils;
@@ -102,14 +97,6 @@ public class MainPreferences extends PreferenceFragmentCompat {
             Runner.MODE_ROOT,
             Runner.MODE_ADB,
             Runner.MODE_NO_ROOT);
-    @StringRes
-    private static final int[] encryptionNames = new int[]{
-            R.string.none,
-            R.string.aes,
-            R.string.rsa,
-            R.string.ecc,
-            R.string.open_pgp_provider
-    };
 
     private static final int[] installLocationNames = new int[]{
             R.string.auto,  // PackageInfo.INSTALL_LOCATION_AUTO
@@ -122,7 +109,6 @@ public class MainPreferences extends PreferenceFragmentCompat {
     private String currentLang;
     @Runner.Mode
     private String currentMode;
-    private int currentCompression;
     private String installerApp;
     private PackageManager pm;
 
@@ -307,77 +293,6 @@ public class MainPreferences extends PreferenceFragmentCompat {
         // Sign apk before install
         ((SwitchPreferenceCompat) Objects.requireNonNull(findPreference("installer_sign_apk")))
                 .setChecked((boolean) AppPref.get(AppPref.PrefKey.PREF_INSTALLER_SIGN_APK_BOOL));
-        // Backup compression method
-        String[] tarTypes = MetadataManager.TAR_TYPES;
-        String[] readableTarTypes = new String[]{"GZip", "BZip2"};
-        currentCompression = ArrayUtils.indexOf(tarTypes, AppPref.get(AppPref.PrefKey.PREF_BACKUP_COMPRESSION_METHOD_STR));
-        Preference compressionMethod = Objects.requireNonNull(findPreference("backup_compression_method"));
-        compressionMethod.setSummary(getString(R.string.compression_method, readableTarTypes[currentCompression == -1 ? 0 : currentCompression]));
-        compressionMethod.setOnPreferenceClickListener(preference -> {
-            new MaterialAlertDialogBuilder(activity)
-                    .setTitle(R.string.pref_compression_method)
-                    .setSingleChoiceItems(readableTarTypes, currentCompression,
-                            (dialog, which) -> currentCompression = which)
-                    .setPositiveButton(R.string.save, (dialog, which) -> {
-                        AppPref.set(AppPref.PrefKey.PREF_BACKUP_COMPRESSION_METHOD_STR, tarTypes[currentCompression]);
-                        compressionMethod.setSummary(getString(R.string.compression_method, readableTarTypes[currentCompression == -1 ? 0 : currentCompression]));
-                    })
-                    .setNegativeButton(R.string.cancel, null)
-                    .show();
-            return true;
-        });
-        // Backup flags
-        BackupFlags flags = BackupFlags.fromPref();
-        ((Preference) Objects.requireNonNull(findPreference("backup_flags"))).setOnPreferenceClickListener(preference -> {
-            new MaterialAlertDialogBuilder(activity)
-                    .setTitle(R.string.backup_options)
-                    .setMultiChoiceItems(BackupFlags.getFormattedFlagNames(activity),
-                            flags.flagsToCheckedItems(),
-                            (dialog, index, isChecked) -> {
-                                if (isChecked) {
-                                    flags.addFlag(BackupFlags.backupFlags.get(index));
-                                } else flags.removeFlag(BackupFlags.backupFlags.get(index));
-                            })
-                    .setPositiveButton(R.string.save, (dialog, which) ->
-                            AppPref.set(AppPref.PrefKey.PREF_BACKUP_FLAGS_INT, flags.getFlags()))
-                    .setNegativeButton(R.string.cancel, null)
-                    .show();
-            return true;
-        });
-        // App usage permission toggle
-        SwitchPreferenceCompat backupKeyStore = Objects.requireNonNull(findPreference("backup_android_keystore"));
-        backupKeyStore.setChecked((boolean) AppPref.get(AppPref.PrefKey.PREF_BACKUP_ANDROID_KEYSTORE_BOOL));
-        // Encryption
-        ((Preference) Objects.requireNonNull(findPreference("encryption"))).setOnPreferenceClickListener(preference -> {
-            CharSequence[] encryptionNamesText = new CharSequence[encryptionNames.length];
-            for (int i = 0; i < encryptionNames.length; ++i) {
-                encryptionNamesText[i] = getString(encryptionNames[i]);
-            }
-            int choice = encModeToIndex((String) AppPref.get(AppPref.PrefKey.PREF_ENCRYPTION_STR));
-            new MaterialAlertDialogBuilder(activity)
-                    .setTitle(R.string.encryption)
-                    .setSingleChoiceItems(encryptionNamesText, choice, (dialog, which) -> {
-                        String encryptionMode = indexToEncMode(which);
-                        switch (encryptionMode) {
-                            case CryptoUtils.MODE_NO_ENCRYPTION:
-                                AppPref.set(AppPref.PrefKey.PREF_ENCRYPTION_STR, encryptionMode);
-                                break;
-                            case CryptoUtils.MODE_AES:
-                            case CryptoUtils.MODE_RSA:
-                            case CryptoUtils.MODE_ECC:
-                                // TODO(12/11/20): Implement encryption options
-                                Toast.makeText(activity, "Not implemented yet.", Toast.LENGTH_SHORT).show();
-                                break;
-                            case CryptoUtils.MODE_OPEN_PGP:
-                                AppPref.set(AppPref.PrefKey.PREF_ENCRYPTION_STR, encryptionMode);
-                                new OpenPgpKeySelectionDialogFragment().show(getParentFragmentManager(), OpenPgpKeySelectionDialogFragment.TAG);
-                        }
-                    })
-                    .setPositiveButton(R.string.ok, null)
-                    .show();
-            return true;
-        });
-
         // About device
         ((Preference) Objects.requireNonNull(findPreference("about_device"))).setOnPreferenceClickListener(preference -> {
             @SuppressLint("InflateParams")
@@ -410,39 +325,6 @@ public class MainPreferences extends PreferenceFragmentCompat {
             }).start();
             return true;
         });
-    }
-
-    @CryptoUtils.Mode
-    private String indexToEncMode(int index) {
-        switch (index) {
-            default:
-            case 0:
-                return CryptoUtils.MODE_NO_ENCRYPTION;
-            case 1:
-                return CryptoUtils.MODE_AES;
-            case 2:
-                return CryptoUtils.MODE_RSA;
-            case 3:
-                return CryptoUtils.MODE_ECC;
-            case 4:
-                return CryptoUtils.MODE_OPEN_PGP;
-        }
-    }
-
-    private int encModeToIndex(@NonNull @CryptoUtils.Mode String mode) {
-        switch (mode) {
-            default:
-            case CryptoUtils.MODE_NO_ENCRYPTION:
-                return 0;
-            case CryptoUtils.MODE_AES:
-                return 1;
-            case CryptoUtils.MODE_RSA:
-                return 2;
-            case CryptoUtils.MODE_ECC:
-                return 3;
-            case CryptoUtils.MODE_OPEN_PGP:
-                return 4;
-        }
     }
 
     @NonNull
