a182b635cf1a5a29e08a5590676794428578d1f1
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2021-01-14 21:56:47 +0600

    [1-ClickOps] Add backup options
    1. Backup all installed apps
    2. Redo existing backups for the installed apps
    3. Backup apps without any previous backups

--- app/src/main/java/io/github/muntashirakon/AppManager/oneclickops/BackupTasksDialogFragment.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/oneclickops/BackupTasksDialogFragment.java
@@ -8,31 +8,94 @@ import android.view.View;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
 import androidx.fragment.app.DialogFragment;
 import io.github.muntashirakon.AppManager.R;
+import io.github.muntashirakon.AppManager.backup.BackupDialogFragment;
+import io.github.muntashirakon.AppManager.backup.BackupUtils;
+import io.github.muntashirakon.AppManager.backup.MetadataManager;
+import io.github.muntashirakon.AppManager.main.ApplicationItem;
+import io.github.muntashirakon.AppManager.types.SearchableMultiChoiceDialogBuilder;
+import io.github.muntashirakon.AppManager.utils.PackageUtils;
 
 public class BackupTasksDialogFragment extends DialogFragment {
     public static final String TAG = "BackupTasksDialogFragment";
 
+    private OneClickOpsActivity activity;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        LayoutInflater inflater = LayoutInflater.from(requireActivity());
+        activity = (OneClickOpsActivity) requireActivity();
+        LayoutInflater inflater = LayoutInflater.from(activity);
         if (inflater == null) return super.onCreateDialog(savedInstanceState);
         @SuppressLint("InflateParams")
         View view = inflater.inflate(R.layout.dialog_backup_tasks, null);
+        // Backup all installed apps
         view.findViewById(R.id.backup_all).setOnClickListener(v -> {
-            // TODO(14/1/21): Backup all apps
+            if (isDetached()) return;
+            activity.mProgressIndicator.show();
+            new Thread(() -> {
+                if (isDetached()) return;
+                HashMap<String, MetadataManager.Metadata> backupMetadata = BackupUtils.getAllBackupMetadata();
+                List<ApplicationItem> applicationItems = new ArrayList<>();
+                List<CharSequence> applicationLabels = new ArrayList<>();
+                for (ApplicationItem item : PackageUtils.getInstalledOrBackedUpApplications(requireContext(), backupMetadata)) {
+                    if (isDetached()) return;
+                    if (item.isInstalled) {
+                        applicationItems.add(item);
+                        applicationLabels.add(item.label);
+                    }
+                }
+                if (isDetached()) return;
+                requireActivity().runOnUiThread(() -> runMultiChoiceDialog(applicationItems, applicationLabels));
+            }).start();
         });
+        // Redo existing backups for the installed apps
         view.findViewById(R.id.redo_existing_backups).setOnClickListener(v -> {
-            // TODO(14/1/21): Redo existing backups
+            if (isDetached()) return;
+            activity.mProgressIndicator.show();
+            new Thread(() -> {
+                if (isDetached()) return;
+                HashMap<String, MetadataManager.Metadata> backupMetadata = BackupUtils.getAllBackupMetadata();
+                List<ApplicationItem> applicationItems = new ArrayList<>();
+                List<CharSequence> applicationLabels = new ArrayList<>();
+                for (ApplicationItem item : PackageUtils.getInstalledOrBackedUpApplications(requireContext(), backupMetadata)) {
+                    if (isDetached()) return;
+                    if (item.isInstalled && item.metadata != null) {
+                        applicationItems.add(item);
+                        applicationLabels.add(item.label);
+                    }
+                }
+                if (isDetached()) return;
+                requireActivity().runOnUiThread(() -> runMultiChoiceDialog(applicationItems, applicationLabels));
+            }).start();
         });
+        // Backup apps without any previous backups
         view.findViewById(R.id.backup_apps_without_backup).setOnClickListener(v -> {
-            // TODO(14/1/21): Backup apps without any previous backups
+            if (isDetached()) return;
+            activity.mProgressIndicator.show();
+            new Thread(() -> {
+                if (isDetached()) return;
+                HashMap<String, MetadataManager.Metadata> backupMetadata = BackupUtils.getAllBackupMetadata();
+                List<ApplicationItem> applicationItems = new ArrayList<>();
+                List<CharSequence> applicationLabels = new ArrayList<>();
+                for (ApplicationItem item : PackageUtils.getInstalledOrBackedUpApplications(requireContext(), backupMetadata)) {
+                    if (isDetached()) return;
+                    if (item.isInstalled && item.metadata == null) {
+                        applicationItems.add(item);
+                        applicationLabels.add(item.label);
+                    }
+                }
+                if (isDetached()) return;
+                requireActivity().runOnUiThread(() -> runMultiChoiceDialog(applicationItems, applicationLabels));
+            }).start();
         });
         view.findViewById(R.id.verify_and_redo_backups).setOnClickListener(v -> {
             // TODO(14/1/21): Verify integrity of the backups and back up the apps whose integrity have failed
@@ -40,10 +103,34 @@ public class BackupTasksDialogFragment extends DialogFragment {
         view.findViewById(R.id.backup_apps_with_changes).setOnClickListener(v -> {
             // TODO(14/1/21): Backup apps with changes
         });
-        return new MaterialAlertDialogBuilder(requireActivity())
+        return new MaterialAlertDialogBuilder(activity)
                 .setView(view)
                 .setTitle(R.string.backup)
                 .setNegativeButton(R.string.cancel, null)
                 .create();
+    }
+
+    @UiThread
+    private void runMultiChoiceDialog(List<ApplicationItem> applicationItems, List<CharSequence> applicationLabels) {
+        if (isDetached()) return;
+        activity.mProgressIndicator.hide();
+        new SearchableMultiChoiceDialogBuilder<>(activity, applicationItems, applicationLabels)
+                .setSelections(applicationItems)
+                .setTitle(R.string.filtered_packages)
+                .setPositiveButton(R.string.restore, (dialog, which, selectedItems) -> {
+                    if (isDetached()) return;
+                    BackupDialogFragment backupDialogFragment = new BackupDialogFragment();
+                    Bundle args = new Bundle();
+                    args.putParcelableArrayList(BackupDialogFragment.ARG_PACKAGE_PAIRS,
+                            PackageUtils.getUserPackagePairs(selectedItems));
+                    args.putInt(BackupDialogFragment.ARG_CUSTOM_MODE, BackupDialogFragment.MODE_BACKUP);
+                    backupDialogFragment.setArguments(args);
+                    backupDialogFragment.setOnActionBeginListener(mode -> activity.mProgressIndicator.show());
+                    backupDialogFragment.setOnActionCompleteListener((mode, failedPackages) -> activity.mProgressIndicator.hide());
+                    if (isDetached()) return;
+                    backupDialogFragment.show(getParentFragmentManager(), BackupDialogFragment.TAG);
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
     }
 }
