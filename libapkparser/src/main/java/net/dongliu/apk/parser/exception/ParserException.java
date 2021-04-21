1cd53a120cab472ce41ccd8d9fa9e27e0aa0154c
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2021-04-21 13:25:10 +0600

    [Refactor] Remove incompatible and unneeded constructor

--- libapkparser/src/main/java/net/dongliu/apk/parser/exception/ParserException.java
+++ libapkparser/src/main/java/net/dongliu/apk/parser/exception/ParserException.java
@@ -18,11 +18,6 @@ public class ParserException extends RuntimeException {
         super(cause);
     }
 
-    public ParserException(String message, Throwable cause, boolean enableSuppression,
-                           boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
     public ParserException() {
     }
 }
