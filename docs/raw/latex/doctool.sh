95fb93cdc7d02d80c835e8e8c77962ef1be72652
gnuhead-chieb <41156994+gnuhead-chieb@users.noreply.github.com>
2021-04-29 09:56:33 +0000

    [latex]Fix several issues 1

---
+++ docs/raw/latex/doctool.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function func_build-html {
+OUTPUT=main.pandoc.html
+pandoc main.tex -c main.css -c custom.css -o $OUTPUT -t html5 -f latex -s --toc -N --section-divs --default-image-extension=png
+
+##Custom Color Fixup
+while read line
+do
+colorset=$(echo $line | sed -e "s/\\definecolor{//" -e "s/}{HTML}{/ /" -e "s/}//")
+colorname=$(echo $colorset | awk '{print $1}')
+colorcode=$(echo $colorset | awk '{print $2}' | sed -e "s/^/#/")
+#echo N $colorname C $colorcode
+sed -i -e "s/style\=\"background-color\: ${colorname}\"/style\=\"background-color\: ${colorcode}\"/g" \
+       -e "s/style\=\"color\: ${colorname}\"/style\=\"color\: ${colorcode}\"/g" $OUTPUT
+done < <(grep "\definecolor" main.tex)
+
+##Alertbox Fixup
+while read line
+do
+sed -i -e "s/--/\" style\=\"border\: 3.0pt solid #/g" $OUTPUT
+done < <(grep "<div class=\"amalert--.*;\">" $OUTPUT)
+
+##Icon Fixup(Need --default-image-extension=png option)
+sed -i -e "s/\<img src\=\"images\/icon.png\" style\=\"width:2cm\"/\<img src\=\"images\/icon-.png\" style\=\"width\:16.69\%\"/g" $OUTPUT
+}
+
+function func_update-xliff {
+# Working
+}
+
+function func_merge-translation {
+# Working
+}
