e4c151fefe8ae1150ee81f120e126297ddcacff0
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2021-05-03 16:43:25 +0600

    Add ParceledListSlice from the AOSP
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

---
+++ app/src/main/java/aosp/android/content/pm/ParceledListSlice.java
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0 AND GPL-3.0-or-later
+
+package aosp.android.content.pm;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Collections;
+import java.util.List;
+
+import io.github.muntashirakon.AppManager.utils.ParcelUtils;
+
+/**
+ * Transfer a large list of Parcelable objects across an IPC.  Splits into
+ * multiple transactions if needed.
+ *
+ * @see BaseParceledListSlice
+ */
+// Copyright 2011 The Android Open Source Project
+@SuppressWarnings("rawtypes")
+public class ParceledListSlice<T extends Parcelable> extends BaseParceledListSlice<T> {
+    public ParceledListSlice(List<T> list) {
+        super(list);
+    }
+
+    private ParceledListSlice(Parcel in) {
+        super(in);
+    }
+
+    public static <T extends Parcelable> ParceledListSlice<T> emptyList() {
+        return new ParceledListSlice<>(Collections.emptyList());
+    }
+
+    @Override
+    public int describeContents() {
+        int contents = 0;
+        final List<T> list = getList();
+        for (T t : list) {
+            contents |= t.describeContents();
+        }
+        return contents;
+    }
+
+    @Override
+    protected void writeElement(T parcelable, Parcel dest, int callFlags) {
+        parcelable.writeToParcel(dest, callFlags);
+    }
+
+    @Override
+    protected void writeParcelableCreator(T parcelable, Parcel dest) {
+        ParcelUtils.writeParcelableCreator(parcelable, dest);
+    }
+
+    @Override
+    protected Parcelable.Creator<?> readParcelableCreator(Parcel from, ClassLoader loader) {
+        return ParcelUtils.readParcelableCreator(from, loader);
+    }
+
+    public static final Parcelable.Creator<ParceledListSlice> CREATOR = new Parcelable.Creator<ParceledListSlice>() {
+        @Override
+        public ParceledListSlice createFromParcel(Parcel in) {
+            return new ParceledListSlice(in);
+        }
+
+        @Override
+        public ParceledListSlice[] newArray(int size) {
+            return new ParceledListSlice[size];
+        }
+    };
+}
---
+++ app/src/main/java/aosp/android/content/pm/StringParceledListSlice.java
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: Apache-2.0 AND GPL-3.0-or-later
+
+package aosp.android.content.pm;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Transfer a large list of Parcelable objects across an IPC.  Splits into
+ * multiple transactions if needed.
+ *
+ * @see BaseParceledListSlice
+ */
+// Copyright 2017 The Android Open Source Project
+public class StringParceledListSlice extends BaseParceledListSlice<String> {
+    public StringParceledListSlice(List<String> list) {
+        super(list);
+    }
+
+    private StringParceledListSlice(Parcel in) {
+        super(in);
+    }
+
+    public static StringParceledListSlice emptyList() {
+        return new StringParceledListSlice(Collections.emptyList());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    protected void writeElement(String parcelable, Parcel reply, int callFlags) {
+        reply.writeString(parcelable);
+    }
+
+    @Override
+    protected void writeParcelableCreator(String parcelable, Parcel dest) {
+    }
+
+    @Override
+    protected Parcelable.Creator<?> readParcelableCreator(Parcel from, ClassLoader loader) {
+        return Parcel.STRING_CREATOR;
+    }
+
+    public static final Parcelable.Creator<StringParceledListSlice> CREATOR =
+            new Parcelable.Creator<StringParceledListSlice>() {
+                @Override
+                public StringParceledListSlice createFromParcel(Parcel in) {
+                    return new StringParceledListSlice(in);
+                }
+
+                @Override
+                public StringParceledListSlice[] newArray(int size) {
+                    return new StringParceledListSlice[size];
+                }
+            };
+}
