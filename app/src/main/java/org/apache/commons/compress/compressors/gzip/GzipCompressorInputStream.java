826cd2d04de4fd6e68a8bc441bddb1a3048c71a4
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2021-05-14 11:59:42 +0600

    [Refactor] Replace asset with IOException

--- app/src/main/java/org/apache/commons/compress/compressors/gzip/GzipCompressorInputStream.java
+++ app/src/main/java/org/apache/commons/compress/compressors/gzip/GzipCompressorInputStream.java
@@ -174,7 +174,9 @@ public class GzipCompressorInputStream extends CompressorInputStream
     }
 
     private boolean init(final boolean isFirstMember) throws IOException {
-        assert isFirstMember || decompressConcatenated;
+        if (!isFirstMember && !decompressConcatenated) {
+            throw new IOException("Initialised without a first member nor the decompressConcatenated is enabled");
+        }
 
         // Check the magic bytes without a possibility of EOFException.
         final int magic0 = in.read();
