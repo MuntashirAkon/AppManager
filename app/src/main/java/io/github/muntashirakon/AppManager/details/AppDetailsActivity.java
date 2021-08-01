25e42bedc5156281e5bfc6a3d07c71d8b2ee0604
d4rkk3y <vuhoanhuy99@gmail.com>
2021-08-01 19:30:01 +0700

    Fix display duplicated uses-permissions,.. Load external apk problem. Signed-off-by: Vu Hoan Huy <vuhoanhuy99@gmail.com>

--- app/src/main/java/io/github/muntashirakon/AppManager/details/AppDetailsActivity.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/details/AppDetailsActivity.java
@@ -91,7 +91,7 @@ public class AppDetailsActivity extends BaseActivity {
                         return;
                     }
                     // Load tabs for the first time
-//                    for (int i = 0; i < mTabTitleIds.length(); ++i) model.load(i);
+                    //for (int i = 0; i < mTabTitleIds.length(); ++i) model.load(i);
                     // Set title
                     ApplicationInfo applicationInfo = packageInfo.applicationInfo;
                     // Set title as the package label
@@ -128,8 +128,7 @@ public class AppDetailsActivity extends BaseActivity {
     private void loadTabs() {
         @AppDetailsFragment.Property int id = viewPager.getCurrentItem();
         Log.e("ADA - " + mTabTitleIds.getText(id), "isPackageChanged called");
-        if (model.getIsExternalApk()) model.load(AppDetailsFragment.APP_INFO);
-        else for (int i = 0; i < mTabTitleIds.length(); ++i) model.load(i);
+        for (int i = 0; i < mTabTitleIds.length(); ++i) model.load(i);
     }
 
     // For tab layout
