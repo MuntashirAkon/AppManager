02887dfadded53f4788bf7e38259b0f71210c61e
Muntashir Al-Islam <muntashirakon@riseup.net>
2021-08-18 17:52:11 +0600

    [Interceptor] Enable copy/paste
    Supported data from the interceptor can be copied, persisted and pasted later.
    Supported data includes primitives such as integer, long integer, floating
    point number, string and their arrays. Other data types are not supported and
    will be dropped when the intent is being flattened to string. The generated
    output contains redundant but useful data such as matching activities and
    result intent (if any) which are completely optional and is dropped when pasted
    from the clipboard.
    
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/intercept/IntentCompat.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/intercept/IntentCompat.java
@@ -6,13 +6,18 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 import static io.github.muntashirakon.AppManager.intercept.AddIntentExtraFragment.ExtraItem;
 import static io.github.muntashirakon.AppManager.intercept.AddIntentExtraFragment.TYPE_BOOLEAN;
@@ -167,53 +172,296 @@ public class IntentCompat {
         }
     }
 
+    @Nullable
+    public static Pair<Integer, String> valueToParsableStringAndType(Object object) {
+        if (object == null) {
+            return new Pair<>(TYPE_NULL, null);
+        } else if (object instanceof String) {
+            return new Pair<>(TYPE_STRING, (String) object);
+        } else if (object instanceof Integer) {
+            return new Pair<>(TYPE_INTEGER, String.valueOf((int) object));
+        } else if (object instanceof Long) {
+            return new Pair<>(TYPE_LONG, String.valueOf((long) object));
+        } else if (object instanceof Float) {
+            return new Pair<>(TYPE_FLOAT, String.valueOf((float) object));
+        } else if (object instanceof Boolean) {
+            return new Pair<>(TYPE_BOOLEAN, String.valueOf((boolean) object));
+        } else if (object instanceof Uri) {
+            return new Pair<>(TYPE_URI, object.toString());
+        } else if (object instanceof ComponentName) {
+            return new Pair<>(TYPE_COMPONENT_NAME, ((ComponentName) object).flattenToString());
+        } else if (object instanceof int[]) {
+            StringBuilder sb = new StringBuilder();
+            int[] list = (int[]) object;
+            if (list.length >= 1) sb.append(list[0]);
+            for (int i = 1; i < list.length; ++i) {
+                sb.append(",").append(list[i]);
+            }
+            return new Pair<>(TYPE_INT_ARR, sb.toString());
+        } else if (object instanceof long[]) {
+            StringBuilder sb = new StringBuilder();
+            long[] list = (long[]) object;
+            if (list.length >= 1) sb.append(list[0]);
+            for (int i = 1; i < list.length; ++i) {
+                sb.append(",").append(list[i]);
+            }
+            return new Pair<>(TYPE_LONG_ARR, sb.toString());
+        } else if (object instanceof float[]) {
+            StringBuilder sb = new StringBuilder();
+            float[] list = (float[]) object;
+            if (list.length >= 1) sb.append(list[0]);
+            for (int i = 1; i < list.length; ++i) {
+                sb.append(",").append(list[i]);
+            }
+            return new Pair<>(TYPE_FLOAT_ARR, sb.toString());
+        } else if (object instanceof String[]) {
+            StringBuilder sb = new StringBuilder();
+            String[] list = (String[]) object;
+            if (list.length >= 1) sb.append(list[0].replace(",", "\\,"));
+            for (int i = 1; i < list.length; ++i) {
+                sb.append(",").append(list[i].replace(",", "\\,"));
+            }
+            return new Pair<>(TYPE_STRING_ARR, sb.toString());
+        } else if (object instanceof List) {
+            @SuppressWarnings("rawtypes")
+            List list = (List) object;
+            if (list.size() == 0) {
+                // Type is lost forever, return null
+                // FIXME: Try to infer type using reflection
+                return new Pair<>(TYPE_NULL, null);
+            }
+            Object item = list.get(0);
+            if (item instanceof Integer) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(item);
+                for (int i = 1; i < list.size(); ++i) {
+                    sb.append(",").append(list.get(i));
+                }
+                return new Pair<>(TYPE_INT_AL, sb.toString());
+            } else if (item instanceof Long) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(item);
+                for (int i = 1; i < list.size(); ++i) {
+                    sb.append(",").append(list.get(i));
+                }
+                return new Pair<>(TYPE_LONG_AL, sb.toString());
+            } else if (item instanceof Float) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(item);
+                for (int i = 1; i < list.size(); ++i) {
+                    sb.append(",").append(list.get(i));
+                }
+                return new Pair<>(TYPE_FLOAT_AL, sb.toString());
+            } else if (item instanceof String) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(((String) item).replace(",", "\\,"));
+                for (int i = 1; i < list.size(); ++i) {
+                    sb.append(",").append(((String) list.get(i)).replace(",", "\\,"));
+                }
+                return new Pair<>(TYPE_STRING_AL, sb.toString());
+            }
+        }
+        return null;
+    }
+
     public static void addToIntent(@NonNull Intent intent, @NonNull ExtraItem extraItem) {
         if (extraItem.keyValue == null && extraItem.type != TYPE_NULL) {
             return;
         }
         switch (extraItem.type) {
-            case AddIntentExtraFragment.TYPE_BOOLEAN:
+            case TYPE_BOOLEAN:
                 intent.putExtra(extraItem.keyName, (boolean) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_COMPONENT_NAME:
+            case TYPE_COMPONENT_NAME:
                 intent.putExtra(extraItem.keyName, (ComponentName) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_FLOAT:
+            case TYPE_FLOAT:
                 intent.putExtra(extraItem.keyName, (float) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_FLOAT_AL:
-            case AddIntentExtraFragment.TYPE_STRING_AL:
-            case AddIntentExtraFragment.TYPE_LONG_AL:
-            case AddIntentExtraFragment.TYPE_INT_AL:
+            case TYPE_FLOAT_AL:
+            case TYPE_STRING_AL:
+            case TYPE_LONG_AL:
+            case TYPE_INT_AL:
                 intent.putExtra(extraItem.keyName, (ArrayList<?>) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_FLOAT_ARR:
+            case TYPE_FLOAT_ARR:
                 intent.putExtra(extraItem.keyName, (float[]) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_INTEGER:
+            case TYPE_INTEGER:
                 intent.putExtra(extraItem.keyName, (int) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_INT_ARR:
+            case TYPE_INT_ARR:
                 intent.putExtra(extraItem.keyName, (int[]) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_LONG:
+            case TYPE_LONG:
                 intent.putExtra(extraItem.keyName, (long) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_LONG_ARR:
+            case TYPE_LONG_ARR:
                 intent.putExtra(extraItem.keyName, (long[]) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_NULL:
+            case TYPE_NULL:
                 intent.putExtra(extraItem.keyName, (String) null);
                 break;
-            case AddIntentExtraFragment.TYPE_STRING:
+            case TYPE_STRING:
                 intent.putExtra(extraItem.keyName, (String) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_STRING_ARR:
+            case TYPE_STRING_ARR:
                 intent.putExtra(extraItem.keyName, (String[]) extraItem.keyValue);
                 break;
-            case AddIntentExtraFragment.TYPE_URI:
+            case TYPE_URI:
                 intent.putExtra(extraItem.keyName, (Uri) extraItem.keyValue);
                 break;
         }
+    }
+
+    @NonNull
+    public static String flattenToString(@NonNull Intent intent) {
+        String action = intent.getAction();
+        String data = intent.getDataString();
+        String type = intent.getType();
+        Set<String> categories = intent.getCategories();
+        ComponentName cn = intent.getComponent();
+        String packageName = intent.getPackage();
+        int flags = intent.getFlags();
+        Bundle extras = intent.getExtras();
+
+        StringBuilder sb = new StringBuilder("VERSION\t").append(1).append("\n");
+        if (action != null) sb.append("ACTION\t").append(action).append("\n");
+        if (data != null) sb.append("DATA\t").append(data).append("\n");
+        if (type != null) sb.append("TYPE\t").append(type).append("\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            String id = intent.getIdentifier();
+            if (id != null) sb.append("IDENTIFIER\t").append(id).append("\n");
+        }
+        if (categories != null) {
+            for (String category : categories) {
+                sb.append("CATEGORY\t").append(category).append("\n");
+            }
+        }
+        if (cn != null) sb.append("COMPONENT\t").append(cn.flattenToString()).append("\n");
+        if (packageName != null) sb.append("PACKAGE\t").append(packageName).append("\n");
+        if (flags != 0) sb.append("FLAGS\t0x").append(Integer.toHexString(flags)).append("\n");
+        if (extras != null) {
+            for (String key : extras.keySet()) {
+                Pair<Integer, String> typeAndString = valueToParsableStringAndType(extras.get(key));
+                if (typeAndString != null) {
+                    sb.append("EXTRA\t").append(key).append("\t").append(typeAndString.first);
+                    if (typeAndString.first != TYPE_NULL) {
+                        sb.append("\t").append(typeAndString.second);
+                    }
+                    sb.append("\n");
+                } // else unsupported bundle item, ignore
+                // TODO: Add support for more items
+            }
+        }
+        return sb.toString();
+    }
+
+    @NonNull
+    public static String describeIntent(@NonNull Intent intent, String prefix) {
+        String action = intent.getAction();
+        String data = intent.getDataString();
+        String type = intent.getType();
+        Set<String> categories = intent.getCategories();
+        ComponentName cn = intent.getComponent();
+        String packageName = intent.getPackage();
+        int flags = intent.getFlags();
+        Bundle extras = intent.getExtras();
+
+        StringBuilder sb = new StringBuilder();
+        if (action != null) sb.append(prefix).append(" ACTION\t").append(action).append("\n");
+        if (data != null) sb.append(prefix).append(" DATA\t").append(data).append("\n");
+        if (type != null) sb.append(prefix).append(" TYPE\t").append(type).append("\n");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            String id = intent.getIdentifier();
+            if (id != null) sb.append(prefix).append(" IDENTIFIER\t").append(id).append("\n");
+        }
+        if (categories != null) {
+            for (String category : categories) {
+                sb.append(prefix).append(" CATEGORY\t").append(category).append("\n");
+            }
+        }
+        if (cn != null) sb.append(prefix).append(" COMPONENT\t").append(cn.flattenToString()).append("\n");
+        if (packageName != null) sb.append(prefix).append(" PACKAGE\t").append(packageName).append("\n");
+        if (flags != 0) sb.append(prefix).append(" FLAGS\t0x").append(Integer.toHexString(flags)).append("\n");
+        if (extras != null) {
+            for (String key : extras.keySet()) {
+                Pair<Integer, String> typeAndString = valueToParsableStringAndType(extras.get(key));
+                if (typeAndString != null) {
+                    sb.append(prefix).append(" EXTRA\t").append(key).append("\t").append(typeAndString.first);
+                    if (typeAndString.first != TYPE_NULL) {
+                        sb.append("\t").append(typeAndString.second);
+                    }
+                    sb.append("\n");
+                } // else unsupported bundle item, ignore
+                // TODO: Add support for more items
+            }
+        }
+        return sb.toString();
+    }
+
+    @Nullable
+    public static Intent unflattenFromString(@NonNull String intentString) {
+        Intent intent = new Intent();
+        String[] lines = intentString.split("\n");
+        Uri data = null;
+        String type = null;
+        for (String line : lines) {
+            if (TextUtils.isEmpty(line)) continue;
+            StringTokenizer tokenizer = new StringTokenizer(line, "\t");
+            if (tokenizer.countTokens() < 2) {
+                // Invalid line
+                return null;
+            }
+            switch (tokenizer.nextToken()) {
+                case "VERSION": {
+                    int version = Integer.decode(tokenizer.nextToken());
+                    if (version != 1) {
+                        // Unsupported version
+                        return null;
+                    }
+                    break;
+                }
+                case "ACTION":
+                    intent.setAction(tokenizer.nextToken());
+                    break;
+                case "DATA":
+                    data = Uri.parse(tokenizer.nextToken());
+                    break;
+                case "TYPE":
+                    type = tokenizer.nextToken();
+                    break;
+                case "IDENTIFIER":
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        intent.setIdentifier(tokenizer.nextToken());
+                    }
+                    break;
+                case "CATEGORY":
+                    intent.addCategory(tokenizer.nextToken());
+                    break;
+                case "COMPONENT":
+                    intent.setComponent(ComponentName.unflattenFromString(tokenizer.nextToken()));
+                    break;
+                case "PACKAGE":
+                    intent.setPackage(tokenizer.nextToken());
+                    break;
+                case "FLAGS":
+                    intent.setFlags(Integer.decode(tokenizer.nextToken()));
+                    break;
+                case "EXTRA": {
+                    ExtraItem item = new ExtraItem();
+                    item.keyName = tokenizer.nextToken();
+                    item.type = Integer.decode(tokenizer.nextToken());
+                    item.keyValue = parseExtraValue(item.type, tokenizer.nextToken());
+                    addToIntent(intent, item);
+                }
+            }
+        }
+        if (data != null) {
+            intent.setDataAndType(data, type);
+        } else if (type != null) {
+            intent.setType(type);
+        }
+        return intent;
     }
 }
