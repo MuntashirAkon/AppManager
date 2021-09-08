c0f42b57362e2fb88a8630f2edf3ff7c7a9f0ff5
Muntashir Al-Islam <muntashirakon@riseup.net>
2021-09-09 00:10:46 +0600

    [Refactor] Fix compatibility issue in TelephonyManager
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- hiddenapi/src/main/java/android/telephony/TelephonyManagerHidden.java
+++ hiddenapi/src/main/java/android/telephony/TelephonyManagerHidden.java
@@ -3,6 +3,9 @@
 package android.telephony;
 
 import android.annotation.Nullable;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import dev.rikka.tools.refine.RefineAs;
 import misc.utils.HiddenUtil;
@@ -13,14 +16,29 @@ public class TelephonyManagerHidden {
      * Returns the unique subscriber ID, for example, the IMSI for a GSM phone
      * for a subscription.
      * Return null if it is unavailable.
+     *
+     * @param subId whose subscriber id is returned
+     * @deprecated Removed in Android 5.1 (Lollipop MR1)
+     */
+    @Deprecated
+    @Nullable
+    public String getSubscriberId(long subId) {
+        return HiddenUtil.throwUOE(subId);
+    }
+
+    /**
+     * Returns the unique subscriber ID, for example, the IMSI for a GSM phone
+     * for a subscription.
+     * Return null if it is unavailable.
      * <p>
      * Requires Permission:
      * {@link android.Manifest.permission#READ_PHONE_STATE READ_PHONE_STATE}
      *
      * @param subId whose subscriber id is returned
      */
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     @Nullable
-    public String getSubscriberId(long subId) {
+    public String getSubscriberId(int subId) {
         return HiddenUtil.throwUOE(subId);
     }
 }
