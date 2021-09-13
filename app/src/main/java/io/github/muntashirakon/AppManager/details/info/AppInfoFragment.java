42f4a22eb9e92c427d245815e6628cd7913278f0
Muntashir Al-Islam <muntashirakon@riseup.net>
2021-09-13 16:15:42 +0600

    [AppInfo] Prevent refreshing if the executor has been shutdown
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/details/info/AppInfoFragment.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/details/info/AppInfoFragment.java
@@ -552,7 +552,7 @@ public class AppInfoFragment extends Fragment implements SwipeRefreshLayout.OnRe
 
     @UiThread
     private void refreshDetails() {
-        if (mainModel == null) return;
+        if (mainModel == null || executor.isShutdown()) return;
         showProgressIndicator(true);
         executor.submit(() -> {
             if (mainModel != null) {
