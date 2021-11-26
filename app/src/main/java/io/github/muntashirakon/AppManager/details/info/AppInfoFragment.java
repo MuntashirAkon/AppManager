57b72e492238e9b0a4e0f552355a7f3974397faa
Muntashir Al-Islam <muntashirakon@riseup.net>
2021-11-26 11:34:15 +0600

    [AppInfo] Add APK selector for scanner if there are splits
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/details/info/AppInfoFragment.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/details/info/AppInfoFragment.java
@@ -1058,40 +1058,7 @@ public class AppInfoFragment extends Fragment implements SwipeRefreshLayout.OnRe
         if (FeatureController.isManifestEnabled()) {
             addToHorizontalLayout(R.string.manifest, R.drawable.ic_package_variant).setOnClickListener(v -> {
                 Intent intent = new Intent(mActivity, ManifestViewerActivity.class);
-                try (ApkFile apkFile = ApkFile.getInstance(mainModel.getApkFileKey())) {
-                    if (apkFile.isSplit()) {
-                        // Display a list of apks
-                        List<ApkFile.Entry> apkEntries = apkFile.getEntries();
-                        CharSequence[] entryNames = new CharSequence[apkEntries.size()];
-                        for (int i = 0; i < apkEntries.size(); ++i) {
-                            entryNames[i] = apkEntries.get(i).toShortLocalizedString(requireActivity());
-                        }
-                        new MaterialAlertDialogBuilder(mActivity)
-                                .setTitle(R.string.select_apk)
-                                .setItems(entryNames, (dialog, which) -> executor.submit(() -> {
-                                    try {
-                                        File file = apkEntries.get(which).getRealCachedFile();
-                                        intent.setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton()
-                                                .getMimeTypeFromExtension("apk"));
-                                        UiThreadHandler.run(() -> startActivity(intent));
-                                    } catch (IOException | RemoteException e) {
-                                        e.printStackTrace();
-                                    }
-                                }))
-                                .setNegativeButton(R.string.cancel, null)
-                                .show();
-                    } else {
-                        // Open directly
-                        if (mainModel.getIsExternalApk()) {
-                            File file = new File(mApplicationInfo.publicSourceDir);
-                            intent.setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton()
-                                    .getMimeTypeFromExtension("apk"));
-                        } else {
-                            intent.putExtra(ManifestViewerActivity.EXTRA_PACKAGE_NAME, mPackageName);
-                        }
-                        startActivity(intent);
-                    }
-                }
+                startActivityForSplit(intent);
             });
         }
         // Set scanner
@@ -1099,9 +1066,7 @@ public class AppInfoFragment extends Fragment implements SwipeRefreshLayout.OnRe
             addToHorizontalLayout(R.string.scanner, R.drawable.ic_baseline_security_24).setOnClickListener(v -> {
                 Intent intent = new Intent(mActivity, ScannerActivity.class);
                 intent.putExtra(ScannerActivity.EXTRA_IS_EXTERNAL, isExternalApk);
-                File file = new File(mApplicationInfo.publicSourceDir);
-                intent.setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton().getMimeTypeFromExtension("apk"));
-                startActivity(intent);
+                startActivityForSplit(intent);
             });
         }
         // Root only features
@@ -1197,6 +1162,44 @@ public class AppInfoFragment extends Fragment implements SwipeRefreshLayout.OnRe
         }
         View v = mHorizontalLayout.getChildAt(0);
         if (v != null) v.requestFocus();
+    }
+
+    private void startActivityForSplit(Intent intent) {
+        if (mainModel == null) return;
+        try (ApkFile apkFile = ApkFile.getInstance(mainModel.getApkFileKey())) {
+            if (apkFile.isSplit()) {
+                // Display a list of apks
+                List<ApkFile.Entry> apkEntries = apkFile.getEntries();
+                CharSequence[] entryNames = new CharSequence[apkEntries.size()];
+                for (int i = 0; i < apkEntries.size(); ++i) {
+                    entryNames[i] = apkEntries.get(i).toShortLocalizedString(requireActivity());
+                }
+                new MaterialAlertDialogBuilder(mActivity)
+                        .setTitle(R.string.select_apk)
+                        .setItems(entryNames, (dialog, which) -> executor.submit(() -> {
+                            try {
+                                File file = apkEntries.get(which).getRealCachedFile();
+                                intent.setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton()
+                                        .getMimeTypeFromExtension("apk"));
+                                UiThreadHandler.run(() -> startActivity(intent));
+                            } catch (IOException | RemoteException e) {
+                                e.printStackTrace();
+                            }
+                        }))
+                        .setNegativeButton(R.string.cancel, null)
+                        .show();
+            } else {
+                // Open directly
+                if (mainModel.getIsExternalApk()) {
+                    File file = new File(mApplicationInfo.publicSourceDir);
+                    intent.setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton()
+                            .getMimeTypeFromExtension("apk"));
+                } else {
+                    intent.putExtra(ManifestViewerActivity.EXTRA_PACKAGE_NAME, mPackageName);
+                }
+                startActivity(intent);
+            }
+        }
     }
 
     @GuardedBy("mListItems")
@@ -1477,6 +1480,7 @@ public class AppInfoFragment extends Fragment implements SwipeRefreshLayout.OnRe
 
     @MainThread
     private void disable() {
+        if (mainModel == null) return;
         try {
             PackageManagerCompat.setApplicationEnabledSetting(mPackageName,
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
