acae32e9bda18fac9126b065b065c825e013031a
Muntashir Al-Islam <muntashirakon@riseup.net>
2022-05-29 12:20:17 +0600

    [Backup] Fix creating symbolic links
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/utils/TarUtils.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/utils/TarUtils.java
@@ -180,7 +180,7 @@ public final class TarUtils {
                         // after the link has been created
                         // TODO: 27/3/22 It might be necessary to check the link if it points to an old APK folder content
                         file.delete();
-                        if (Objects.requireNonNull(file.getFile()).createNewSymlink(linkName)) {
+                        if (!Objects.requireNonNull(file.getFile()).createNewSymlink(linkName)) {
                             throw new IOException("Couldn't create symbolic link " + file + " pointing to " + linkName);
                         }
                         continue;  // links do not need permission fixes
