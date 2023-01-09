d722368fecd9a646d0aa25c2b7e22234771581eb
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-01-09 14:34:42 +0600

    [Refactor] Move campaign logic to a separate class

---
+++ app/src/main/java/io/github/muntashirakon/AppManager/self/life/FundingCampaignChecker.java
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package io.github.muntashirakon.AppManager.self.life;
+
+public class FundingCampaignChecker {
+    private static final long FUNDING_CAMPAIGN_START = 1671796800000L;
+    private static final long FUNDING_CAMPAIGN_END = 1680350400000L;
+
+    public static boolean campaignRunning() {
+        long currentTime = System.currentTimeMillis();
+        return currentTime >= FUNDING_CAMPAIGN_START && currentTime <= FUNDING_CAMPAIGN_END;
+    }
+}
