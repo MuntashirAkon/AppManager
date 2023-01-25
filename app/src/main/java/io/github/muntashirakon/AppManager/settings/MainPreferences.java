9a52bfb004ac792305aacd0bded57320b09bf840
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-01-25 10:08:01 +0600

    [Settings] Disable wireless debugging, not ADB over TCP in API < 30
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/settings/MainPreferences.java
@@ -107,7 +107,7 @@ public class MainPreferences extends PreferenceFragment {
                     .setTitle(R.string.pref_mode_of_operations)
                     .setSelection(currentMode)
                     .addDisabledItems(Build.VERSION.SDK_INT < Build.VERSION_CODES.R ?
-                            Collections.singletonList(Ops.MODE_ADB_OVER_TCP) : Collections.emptyList())
+                            Collections.singletonList(Ops.MODE_ADB_WIFI) : Collections.emptyList())
                     .setPositiveButton(R.string.apply, (dialog, which, selectedItem) -> {
                         currentMode = selectedItem;
                         if (Ops.MODE_ADB_OVER_TCP.equals(currentMode)) {
