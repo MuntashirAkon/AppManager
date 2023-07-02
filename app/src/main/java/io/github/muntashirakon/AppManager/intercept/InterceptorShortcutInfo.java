dc78b3565f7847468bf978bb98ffadfe3efab73b
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-07-02 22:50:17 +0600

    [Interceptor] Prevent the app from crashing on creating a shortcut with invalid extras
    Shortcuts support only 11 types of data structures. Intents, on the other hand,
    support any type of data structures. When a shortcut creation is requested, the
    unsupported data structures are removed from the Intent to avoid crashes.
    
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/intercept/InterceptorShortcutInfo.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/intercept/InterceptorShortcutInfo.java
@@ -4,18 +4,25 @@ package io.github.muntashirakon.AppManager.intercept;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Parcel;
+import android.os.PersistableBundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.os.ParcelCompat;
 
+import io.github.muntashirakon.AppManager.logs.Log;
 import io.github.muntashirakon.AppManager.shortcut.ShortcutInfo;
 
 public class InterceptorShortcutInfo extends ShortcutInfo {
+    public static final String TAG = InterceptorShortcutInfo.class.getSimpleName();
+
     private final Intent intent;
 
     public InterceptorShortcutInfo(@NonNull Intent intent) {
-        this.intent = intent;
+        this.intent = new Intent(intent);
+        fixIntent(this.intent);
     }
 
     protected InterceptorShortcutInfo(Parcel in) {
@@ -45,4 +52,34 @@ public class InterceptorShortcutInfo extends ShortcutInfo {
             return new InterceptorShortcutInfo[size];
         }
     };
+
+    @SuppressWarnings("deprecation")
+    private static void fixIntent(@NonNull Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras == null) {
+            // Nothing to do
+            return;
+        }
+        // Shortcuts use PersistableBundle for extras which only support 12 types
+        for (String key : extras.keySet()) {
+            Object value = extras.get(key);
+            if (!isValidType(value)) {
+                // Not a valid type, remove it from intent
+                Log.w(TAG, "Removing unsupported key " + key + " (class: " + value.getClass().getName() + ", value: " + value + ")");
+                intent.removeExtra(key);
+            }
+        }
+    }
+
+    /**
+     * @see PersistableBundle#isValidType(Object)
+     */
+    private static boolean isValidType(@Nullable Object value) {
+        return (value instanceof Integer) || (value instanceof Long) ||
+                (value instanceof Double) || (value instanceof String) ||
+                (value instanceof int[]) || (value instanceof long[]) ||
+                (value instanceof double[]) || (value instanceof String[]) ||
+                (value instanceof PersistableBundle) || (value == null) ||
+                (value instanceof Boolean) || (value instanceof boolean[]);
+    }
 }
