e89b455bdc4a70ba8d573b2360a3a5799833cbf4
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-07-15 03:43:04 +0600

    [Debloater] Update package labels and suggestions
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- scripts/make_debloat_list.php
+++ scripts/make_debloat_list.php
@@ -22,7 +22,6 @@ foreach (list_files(REPO_DIR) as $filename) {
         continue;
     } else fprintf(STDERR, "Adding $filename\n");
     foreach ($list as $item) {
-        verify_item($item);
         if ($item['removal'] == 'unsafe') {
             fprintf(STDERR, "Removing unsafe item {$item['id']}\n");
             continue;
@@ -33,89 +32,3 @@ foreach (list_files(REPO_DIR) as $filename) {
 }
 
 file_put_contents($target_file, json_encode($debloat_list, JSON_UNESCAPED_UNICODE));
-
-function verify_item(array $item): void {
-    // `id` is a string
-    if (gettype($item['id']) != 'string') {
-        fprintf(STDERR, "Expected `id` field to be a string, found: " . gettype($item['id']) . "\n");
-    }
-    // `label` is an optional string
-    if (isset($item['label']) && gettype($item['label']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `label` field to be a string, found: " . gettype($item['label']) . "\n");
-    }
-    // `dependencies` is an optional string[]
-    if (isset($item['dependencies'])) {
-        if (gettype($item['dependencies']) != 'array') {
-            fprintf(STDERR, "{$item['id']}: Expected `dependencies` field to be an array, found: " . gettype($item['dependencies']) . "\n");
-        } else {
-            foreach ($item['dependencies'] as $dependency) {
-                if (gettype($dependency) != 'string') {
-                    fprintf(STDERR, "{$item['id']}: Expected `dependencies` items to be a string, found: " . gettype($dependency) . "\n");
-                }
-            }
-        }
-    }
-    // `required_by` is an optional string[]
-    if (isset($item['required_by'])) {
-        if (gettype($item['required_by']) != 'array') {
-            fprintf(STDERR, "{$item['id']}: Expected `required_by` field to be an array, found: " . gettype($item['required_by']) . "\n");
-        } else {
-            foreach ($item['required_by'] as $required_by) {
-                if (gettype($required_by) != 'string') {
-                    fprintf(STDERR, "{$item['id']}: Expected `required_by` items to be a string, found: " . gettype($required_by) . "\n");
-                }
-            }
-        }
-    }
-    // `tags` is an optional string[]
-    if (isset($item['tags'])) {
-        if (gettype($item['tags']) != 'array') {
-            fprintf(STDERR, "{$item['id']}: Expected `tags` field to be an array, found: " . gettype($item['tags']) . "\n");
-        } else {
-            foreach ($item['tags'] as $tag) {
-                if (gettype($tag) != 'string') {
-                    fprintf(STDERR, "{$item['id']}: Expected `tags` items to be a string, found: " . gettype($tag) . "\n");
-                } else if (!in_array($tag, SUPPORTED_TAGS)) {
-                    fprintf(STDERR, "{$item['id']}: Invalid `tag`: $tag\n");
-                }
-            }
-        }
-    }
-    // `description` is a string
-    if (gettype($item['description']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `description` field to be a string, found: " . gettype($item['description']) . "\n");
-    }
-    // `web` is an optional string[]
-    if (isset($item['web'])) {
-        if (gettype($item['web']) != 'array') {
-            fprintf(STDERR, "{$item['id']}: Expected `web` field to be an array, found: " . gettype($item['web']) . "\n");
-        } else {
-            foreach ($item['web'] as $site) {
-                if (gettype($site) != 'string') {
-                    fprintf(STDERR, "{$item['id']}: Expected `web` items to be a string, found: " . gettype($site) . "\n");
-                }
-            }
-        }
-    }
-    // `removal` is a string
-    if (gettype($item['removal']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `removal` field to be a string, found: " . gettype($item['removal']) . "\n");
-    } else if (!in_array($item['removal'], SUPPORTED_REMOVAL_TYPES)) {
-        fprintf(STDERR, "{$item['id']}: Invalid `removal` type: {$item['removal']}\n");
-    }
-    // `warning` is an optional string
-    if (isset($item['warning']) && gettype($item['warning']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `warning` field to be a string, found: " . gettype($item['warning']) . "\n");
-    }
-    // `warning` is an optional string
-    if (isset($item['suggestions'])) {
-        if (gettype($item['suggestions']) != 'string') {
-            fprintf(STDERR, "{$item['id']}: Expected `suggestions` field to be a string, found: " . gettype($item['suggestions']) . "\n");
-        } else {
-            $suggestion_file = REPO_DIR . '/suggestions/' . $item['suggestions'] . '.json';
-            if (!file_exists($suggestion_file)) {
-                fprintf(STDERR, "{$item['id']}: Suggestion ID ({$item['suggestions']}) does not exist.\n");
-            }
-        }
-    }
-}
--- scripts/make_suggestions.php
+++ scripts/make_suggestions.php
@@ -21,37 +21,9 @@ foreach (list_files(SUGGESTIONS_DIR) as $filename) {
         continue;
     } else fprintf(STDERR, "Adding $filename\n");
     foreach ($single_suggestion_list as $suggestion) {
-        validate_suggestion_item($suggestion);
         $suggestion['_id'] = $suggestion_id;
         $suggestions[] = $suggestion;
     }
 }
 
 file_put_contents($target_file, json_encode($suggestions, JSON_UNESCAPED_UNICODE));
-
-function validate_suggestion_item(array $item): void {
-    // `id` is a string
-    if (gettype($item['id']) != 'string') {
-        fprintf(STDERR, "Expected `id` field to be a string, found: " . gettype($item['id']) . "\n");
-    }
-    // `label` is a string
-    if (gettype($item['label']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `label` field to be a string, found: " . gettype($item['label']) . "\n");
-    }
-    // `reason` is an optional string
-    if (isset($item['reason']) && gettype($item['reason']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `reason` field to be a string, found: " . gettype($item['reason']) . "\n");
-    }
-    // `source` is an optional string
-    if (isset($item['source'])) {
-        if (gettype($item['source']) != 'string') {
-            fprintf(STDERR, "{$item['id']}: Expected `source` field to be a string, found: " . gettype($item['source']) . "\n");
-        } else if (!preg_match("/^[fgas]+$/", $item['source'])) {
-            fprintf(STDERR, "{$item['id']}: Expected `source` field to contain one or more from `fgas`, found: {$item['source']}\n");
-        }
-    }
-    // `repo` is a string
-    if (gettype($item['repo']) != 'string') {
-        fprintf(STDERR, "{$item['id']}: Expected `repo` field to be a string, found: " . gettype($item['repo']) . "\n");
-    }
-}
