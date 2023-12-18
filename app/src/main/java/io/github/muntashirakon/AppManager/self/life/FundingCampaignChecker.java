8dc4dd29da1e9ab2da14ef57fe9d26f4f2d39d9e
Muntashir Al-Islam <muntashirakon@riseup.net>
2023-12-18 23:40:46 +0600

    Update funding campaign dates
    Start date: 21 Dec 2023
    End date: 1 April 2024
    
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/self/life/FundingCampaignChecker.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/self/life/FundingCampaignChecker.java
@@ -3,8 +3,8 @@
 package io.github.muntashirakon.AppManager.self.life;
 
 public class FundingCampaignChecker {
-    private static final long FUNDING_CAMPAIGN_START = 1671796800000L;
-    private static final long FUNDING_CAMPAIGN_END = 1680350400000L;
+    private static final long FUNDING_CAMPAIGN_START = 1703160000000L;
+    private static final long FUNDING_CAMPAIGN_END = 1711972800000L;
 
     public static boolean campaignRunning() {
         long currentTime = System.currentTimeMillis();
