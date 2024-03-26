f0552f2c63dd5df7a24f0e139c88ea71e5885bbe
Muntashir Al-Islam <muntashirakon@riseup.net>
2024-03-27 00:25:42 +0600

    [RunningApps] Auto-refresh apps in every 10 seconds
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/runningapps/AppProcessItem.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/runningapps/AppProcessItem.java
@@ -41,25 +41,6 @@ public class AppProcessItem extends ProcessItem {
     };
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof AppProcessItem)) {
-            if (o instanceof ProcessItem) {
-                return super.equals(o);
-            }
-            return false;
-        }
-        if (!super.equals(o)) return false;
-        AppProcessItem that = (AppProcessItem) o;
-        return packageInfo.equals(that.packageInfo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), packageInfo);
-    }
-
-    @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
         dest.writeParcelable(packageInfo, flags);
