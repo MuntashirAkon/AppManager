ce13a8551b97da9ee6504e232aa380b671cadd03
PQY <pangqingyuan@casachina.com.cn>
2024-05-27 16:41:01 +0800

    ace2v

--- app/src/main/java/io/github/muntashirakon/AppManager/self/BootReceiver.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/self/BootReceiver.java
@@ -5,13 +5,15 @@ package io.github.muntashirakon.AppManager.self;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+import io.github.muntashirakon.AppManager.runner.Runner;
 import io.github.muntashirakon.AppManager.self.filecache.InternalCacheCleanerService;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Runner.runCommand(new String[]{"/system/bin/su","-c", "/system/bin/swapoff", "/dev/block/zram0"});
+            Runner.runCommand(new String[]{"/system/bin/su","-c", "/system/bin/swapoff", "/dev/block/zram1"});
             InternalCacheCleanerService.scheduleAlarm(context.getApplicationContext());
         }
     }
