a7100d19fe4c7530f3cc166b4f3ad4817a6bc8f3
Muntashir Al-Islam <muntashirakon@riseup.net>
2025-02-14 23:50:29 -0800

    [Debloater] Update bloatware and suggestions
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- scripts/make_debloat_list.php
+++ scripts/make_debloat_list.php
@@ -26,9 +26,12 @@ foreach (list_files(REPO_DIR) as $filename) {
             fprintf(STDERR, "Removing unsafe item {$item['id']}\n");
             continue;
         }
+        if (isset($item['suppress'])) {
+            unset($item['suppress']);
+        }
         $item['type'] = $type;
         $debloat_list[] = $item;
     }
 }
 
-file_put_contents($target_file, json_encode($debloat_list, JSON_UNESCAPED_UNICODE));
+file_put_contents($target_file, json_encode($debloat_list, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
