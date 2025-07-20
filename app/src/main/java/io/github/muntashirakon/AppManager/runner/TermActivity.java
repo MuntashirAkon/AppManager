8bb761a45ffeea83eaae2441abeb00eefba006bb
Muntashir Al-Islam <muntashirakon@riseup.net>
2025-07-19 19:44:45 -0700

    [Terminal] Display commands in bold with $ prepended
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>
    (cherry picked from commit a1713d0226e9f4c6345638cd3ef3b0184686a75d)

--- app/src/main/java/io/github/muntashirakon/AppManager/runner/TermActivity.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/runner/TermActivity.java
@@ -2,20 +2,24 @@
 
 package io.github.muntashirakon.AppManager.runner;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.text.Editable;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
-import androidx.annotation.AnyThread;
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
@@ -35,6 +39,7 @@ public class TermActivity extends BaseActivity {
     private Process mProc;
     private OutputStream mProcessOutputStream;
     private PowerManager.WakeLock mWakeLock;
+    private boolean mCommandInProgress;
     private final ExecutorService mExecutor = Executors.newFixedThreadPool(3);
 
     @Override
@@ -43,9 +48,13 @@ public class TermActivity extends BaseActivity {
         setSupportActionBar(findViewById(R.id.toolbar));
         mCommandInput = findViewById(R.id.command_input);
         mCommandOutput = findViewById(R.id.command_output);
+        mCommandOutput.setText("", TextView.BufferType.EDITABLE);
         mCommandInput.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                String command = Objects.requireNonNull(mCommandInput.getText()) + "\n";
+                String command = Objects.requireNonNull(mCommandInput.getText()).toString();
+                appendBoldOutput((mCommandInProgress ? "  " : "$ ") + command);
+                mCommandInProgress = command.endsWith("\\");
+                appendOutput("\n");
                 if (mProcessOutputStream != null) {
                     if (!ProcessCompat.isAlive(mProc)) {
                         // Process is dead
@@ -53,6 +62,7 @@ public class TermActivity extends BaseActivity {
                     }
                     try {
                         mProcessOutputStream.write(command.getBytes(StandardCharsets.UTF_8));
+                        mProcessOutputStream.write("\n".getBytes(StandardCharsets.UTF_8));
                         mProcessOutputStream.flush();
                     } catch (Throwable e) {
                         e.printStackTrace();
@@ -89,12 +99,13 @@ public class TermActivity extends BaseActivity {
                 mProc = ProcessCompat.exec(new String[]{"sh"}/*, new String[]{"TERM=xterm-256color"}*/);
                 mProcessOutputStream = new BufferedOutputStream(mProc.getOutputStream());
                 mExecutor.submit(() -> {
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(mProc.getInputStream()))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
+                    try (InputStream in = mProc.getInputStream()) {
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = in.read(buffer)) != -1) {
                             synchronized (mLock) {
-                                String finalLine = line;
-                                runOnUiThread(() -> mCommandOutput.append(finalLine + "\n"));
+                                String chunk = new String(buffer, 0, len, StandardCharsets.UTF_8);
+                                runOnUiThread(() -> appendOutput(chunk));
                             }
                         }
                     } catch (Throwable e) {
@@ -102,12 +113,13 @@ public class TermActivity extends BaseActivity {
                     }
                 });
                 mExecutor.submit(() -> {
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(mProc.getErrorStream()))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
+                    try (InputStream in = mProc.getErrorStream()) {
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = in.read(buffer)) != -1) {
                             synchronized (mLock) {
-                                String finalLine = line;
-                                runOnUiThread(() -> mCommandOutput.append(finalLine + "\n"));
+                                String chunk = new String(buffer, 0, len, StandardCharsets.UTF_8);
+                                runOnUiThread(() -> appendOutput(chunk));
                             }
                         }
                     } catch (Throwable e) {
@@ -123,8 +135,16 @@ public class TermActivity extends BaseActivity {
         });
     }
 
-    @AnyThread
-    private void appendOutput(String line) {
-        mCommandOutput.append(line + "\n");
+    @MainThread
+    private void appendOutput(String text) {
+        mCommandOutput.append(text);
+    }
+
+    @MainThread
+    private void appendBoldOutput(String boldText) {
+        Editable editable = mCommandOutput.getEditableText();
+        int start = editable.length();
+        editable.append(boldText);
+        editable.setSpan(new StyleSpan(Typeface.BOLD), start, editable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 }
