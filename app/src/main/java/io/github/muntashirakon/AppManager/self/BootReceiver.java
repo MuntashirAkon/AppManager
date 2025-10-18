6dbf22bc54d2dc1021fee41a030592f86d1f3ca0
Muntashir Al-Islam <muntashirakon@riseup.net>
2025-10-18 01:30:31 -0700

    Wait for Wi-Fi networks on boot in "wireless debugging" mode
    Signed-off-by: Muntashir Al-Islam <muntashirakon@riseup.net>

--- app/src/main/java/io/github/muntashirakon/AppManager/self/BootReceiver.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/self/BootReceiver.java
@@ -6,12 +6,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.core.content.ContextCompat;
+
 import io.github.muntashirakon.AppManager.self.filecache.InternalCacheCleanerService;
+import io.github.muntashirakon.AppManager.servermanager.WifiWaitService;
+import io.github.muntashirakon.AppManager.settings.Ops;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            if (Ops.getMode().equals(Ops.MODE_ADB_WIFI)) {
+                // Connect ADB
+                Intent serviceIntent = new Intent(context, WifiWaitService.class);
+                ContextCompat.startForegroundService(context, serviceIntent);
+            }
+            // Schedule cache cleaning
             InternalCacheCleanerService.scheduleAlarm(context.getApplicationContext());
         }
     }
